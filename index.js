6f77317b1faf50d5e0575c53c9b5baf72e3822ae
AndrewSlama <117046824+AndrewSlama@users.noreply.github.com>
2023-03-05 11:58:26 -0600

    added functions and time

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener('click', btn); //does something when start button clicked
+document.querySelector("#start-btn"); //selects start button
+
+function setTime() {
+    var timerInterval = setInterval(function()) {
+        secondsLeft--;
+        
+    }
+}
