3e0c667f111774fed8cf54bcf3c97ba667dc2a0e
AndrewSlama <117046824+AndrewSlama@users.noreply.github.com>
2023-03-05 16:47:00 -0600

    messing with timer

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 document.querySelector("#start-btn"); //selects start button
 let hiddenBtn = document.getElementById('hidden').innerHTML;
-let visibleBtn = document.getElementById('button').innerHTML;
+let startButton = document.getElementById("#start-btn").innerHTML;
 
-
+var secondsLeft = 40;
 var answer = [];
 var question = [];
 var ifQuestion = false;
 var ifAnswer = false;
 
-document.addEventListener('click', btn) {
-    if(visibleBtn === "click")
+document.addEventListener('click', startButton) {
+    let startGo = setTimeout(function, 4000);
     
     }
-}//does something when button is clicked
-
-
-function setTime() {
-    var timerInterval = setInterval(function()) {
-        secondsLeft--;
-
-    }
 }
+
+
+function startTimeer() {
+    var seconds = 40;
+    setInterval(function()) {
+        seconds--;
+        if (seconds >= 0) {
+            span = document.getElementById("start-btn");
+            span.innerHTML = seconds;
+        }
+        if (seconds === 0) {
+            alert("Game Over");
+            clearInterval(seconds);
+        }
+    }, 4000;
+}
+function startTimeer();
+{
+    document.getElementById('start-btn');
+    startTimeer();
+};
+   
