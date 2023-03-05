6840143a30333ee9263a32e8cc5f82a92fe52472
AndrewSlama <117046824+AndrewSlama@users.noreply.github.com>
2023-03-05 16:14:09 -0600

    added buttons hide

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 document.addEventListener('click', btn); //does something when start button clicked
 document.querySelector("#start-btn"); //selects start button
+let hiddenBtn = document.getElementById('hidden');
+let visibleBtn = document.getElementById('button');
+
+var answer = true;
+var question = true;
+var ifQuestion = false;
+var ifAnswer = false;
+
 
 function setTime() {
     var timerInterval = setInterval(function()) {
         secondsLeft--;
-        
+
     }
 }
